f59c3809538cb7cbd995c5a021c19a008de779d9
tmetreveli <tornike.metreveli07p@geolab.edu.ge>
2024-04-10 17:59:58 +0400

    defiend verbose_names for model fields

--- BasicApp/market/models.py
+++ BasicApp/market/models.py
@@ -1,14 +1,19 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
 
 # Create your models here.
 class Book(models.Model):
-    name = models.CharField(max_length=255)
-    page_count = models.IntegerField()
-    category = models.CharField(max_length=255)
-    author_name = models.CharField(max_length=255)
-    price = models.DecimalField(decimal_places=3, max_digits=5)
-    image = models.ImageField()
+    name = models.CharField(max_length=255, verbose_name=_("Book title"))
+    page_count = models.IntegerField(verbose_name=_("Page Count"))
+    category = models.CharField(max_length=255, verbose_name=_("Book category"))
+    author_name = models.CharField(max_length=255, verbose_name=_("Author Name"))
+    price = models.DecimalField(decimal_places=3, max_digits=5, verbose_name=_("Price"))
+    image = models.ImageField(verbose_name=_("Image"))
 
     def __str__(self):
         return self.name
+
+    class Meta:
+        verbose_name = _('Book')
+        verbose_name_plural = _('Books')
