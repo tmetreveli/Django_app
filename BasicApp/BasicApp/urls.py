3224b1e404c7dc293df6a5d3e8d435e49266f914
tmetreveli <tornike.metreveli07p@geolab.edu.ge>
2024-04-11 12:54:28 +0400

    updated index.html template

--- BasicApp/BasicApp/urls.py
+++ BasicApp/BasicApp/urls.py
@@ -21,5 +21,5 @@ from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include("market.urls"))
+    path('', include("market.urls", namespace='market'))
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- BasicApp/market/urls.py
+++ BasicApp/market/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 from .views import list_books, book_detail, show, book_detailed_view
 
+app_name = 'market'
+
 urlpatterns = [
     path('book_list', list_books),
     path('book_list/<int:product_id>', book_detail),
     path('', show),
-    path('details/<int:id>', book_detailed_view)
+    path('details/<int:id>', book_detailed_view, name='book_detailed_view')
 ]
