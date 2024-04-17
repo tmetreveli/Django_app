f2676540fbbeb2f947a61586d2250f391e0ed3cf
tmetreveli <tornike.metreveli07p@geolab.edu.ge>
2024-04-17 22:02:08 +0400

    fixed minor issues

---
+++ BasicApp/market/choices.py
@@ -0,0 +1,12 @@
+CATEGORY_CHOICES = [
+        ('FANTASY', 'fantasy'),
+        ('ROMANCE', 'romance'),
+        ('DOCUMENTARY', 'documentary'),
+        ('UNDEFINED', 'undefined')
+    ]
+
+COVER_CHOICES = [
+            ('PAPERBACK', 'Paperback'),
+            ('HARDCOVER', 'Hardcover'),
+            ('SPEC', 'Spec'),
+        ]
--- BasicApp/market/models.py
+++ BasicApp/market/models.py
@@ -1,9 +1,10 @@
 from django.db import models
 from django.utils.translation import gettext_lazy as _
+from market.choices import CATEGORY_CHOICES, COVER_CHOICES
 
 
 class Author(models.Model):
-    name = models.CharField(max_length=100)
+    name = models.CharField(max_length=100, verbose_name=_("Author name"))
 
     def __str__(self):
         return self.name
@@ -14,13 +15,7 @@ class Author(models.Model):
 
 
 class Category(models.Model):
-    CATEGORY_CHOICES = [
-        ('FANTASY', 'fantasy'),
-        ('ROMANCE', 'romance'),
-        ('DOCUMENTARY', 'documentary'),
-        ('UNDEFINED', 'undefined')
-    ]
-    name = models.CharField(max_length=11, choices=CATEGORY_CHOICES, default='PAPERBACK')
+    name = models.CharField(max_length=11, choices=CATEGORY_CHOICES, default='PAPERBACK', verbose_name=_("Category name"))
 
     def __str__(self):
         return self.name
@@ -31,21 +26,14 @@ class Category(models.Model):
 
 # Create your models here.
 class Book(models.Model):
-    COVER_CHOICES = [
-            ('PAPERBACK', 'Paperback'),
-            ('HARDCOVER', 'Hardcover'),
-            ('SPEC', 'Spec'),
-        ]
-
+    author = models.ForeignKey(Author, on_delete=models.CASCADE, null=True, blank=True, verbose_name=_("Book Author"))
+    category = models.ManyToManyField(Category, default='undefined', verbose_name=_("Book Category"))
     name = models.CharField(max_length=255, verbose_name=_("Book title"))
     page_count = models.IntegerField(verbose_name=_("Page Count"))
-    # category = models.CharField(max_length=255, verbose_name=_("Book category"))
-    # author_name = models.CharField(max_length=255, verbose_name=_("Author Name"))
     price = models.DecimalField(decimal_places=2, max_digits=10, verbose_name=_("Price"))
     image = models.ImageField(upload_to='images/', verbose_name=_("Image"))
-    author = models.ForeignKey(Author, on_delete=models.CASCADE, null=True, blank=True)
-    cover = models.CharField(max_length=9, choices=COVER_CHOICES, default='PAPERBACK')
-    category = models.ManyToManyField(Category, default='undefined')
+    cover = models.CharField(max_length=9, choices=COVER_CHOICES, default='PAPERBACK', verbose_name=_("Book cover"))
+    
 
     def __str__(self):
         return self.name
